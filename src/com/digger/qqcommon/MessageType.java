8826b8e286a2502b09fd1b2dce312ff191a9b3f8
708828375 <708828375@qq.com>
2021-09-13 00:06:32 +0800

    Initial commit

---
+++ src/com/digger/qqcommon/MessageType.java
@@ -0,0 +1,12 @@
+package com.digger.qqcommon;
+
+/**
+ * @Description : 消息的类型
+ * @Author : 孙梦琼
+ * @Date : 2021/9/12 22:02
+ * @Version : 1.0
+ **/
+public interface MessageType {
+    String MESSAGE_LOGIN_SUCCEED = "1";//表示登录成功
+    String MESSAGE_LOGIN_FAIL = "2";//表示登陆失败
+}
---
+++ src/com/digger/qqcommon/User.java
@@ -0,0 +1,43 @@
+package com.digger.qqcommon;
+
+import jdk.internal.org.objectweb.asm.commons.SerialVersionUIDAdder;
+
+import java.io.Serializable;
+
+/**
+ * @Description : 表示一个用户信息
+ * @Author : 孙梦琼
+ * @Date : 2021/9/12 21:48
+ * @Version : 1.0
+ **/
+public class User implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+    private String id;
+    private String userName;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
