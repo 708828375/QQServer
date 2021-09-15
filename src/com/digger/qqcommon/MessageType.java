890af75ee832ed40169e99dd2a6e80db88259b24
708828375 <708828375@qq.com>
2021-09-15 11:32:22 +0800

    请求在线好友列表1

--- src/com/digger/qqcommon/MessageType.java
+++ src/com/digger/qqcommon/MessageType.java
@@ -9,4 +9,8 @@ package com.digger.qqcommon;
 public interface MessageType {
     String MESSAGE_LOGIN_SUCCEED = "1";//表示登录成功
     String MESSAGE_LOGIN_FAIL = "2";//表示登陆失败
+    String MESSAGE_COMM_MES = "3"; //普通的信息对象
+    String MESSAGE_GET_ONLINE_FRIEND = "4"; //要求返回在线好友列表
+    String MESSAGE_RET_ONLINE_FRIEND = "5"; //返回在线用户列表
+    String MESSAGE_CLIENT_EXIT = "6"; //客户端请求退出
 }
